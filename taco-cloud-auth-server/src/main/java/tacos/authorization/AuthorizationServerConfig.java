0c12978fe25308f98ec58c20688a0026495afbe7
Utkarsh <ignition3951@gmail.com>
2024-08-07 16:59:10 +0530

    Implement authorization changes

--- taco-cloud-auth-server/src/main/java/tacos/authorization/AuthorizationServerConfig.java
+++ taco-cloud-auth-server/src/main/java/tacos/authorization/AuthorizationServerConfig.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -42,37 +41,66 @@ public class AuthorizationServerConfig {
 		return new BCryptPasswordEncoder();
 	}
 
+//	@Bean
+//	@Order(Ordered.HIGHEST_PRECEDENCE)
+//	public SecurityFilterChain authorizationSecurityFilterChain(HttpSecurity httpSecurity,
+//			RegisteredClientRepository registeredClientRepository,
+//			AuthorizationServerSettings authorizationServerSettings) throws Exception {
+////		org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+////				.applyDefaultSecurity(httpSecurity);
+//		httpSecurity.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults());
+//		httpSecurity
+//				.exceptionHandling((exceptions) -> exceptions
+//						.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")));
+////				.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
+//
+//
+//		return httpSecurity.formLogin(Customizer.withDefaults()).build();
+//	}
 	@Bean
-	@Order(Ordered.HIGHEST_PRECEDENCE)
-	public SecurityFilterChain authorizationSecurityFilterChain(HttpSecurity httpSecurity,
-			RegisteredClientRepository registeredClientRepository,
-			AuthorizationServerSettings authorizationServerSettings) throws Exception {
-		org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
-				.applyDefaultSecurity(httpSecurity);
-		httpSecurity
-				.exceptionHandling((exceptions) -> exceptions
-						.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")))
-				.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
-
+	@Order(1)
+	public SecurityFilterChain asFilterChain(HttpSecurity httpSecurity) throws Exception {
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
 		httpSecurity.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults());
-		return httpSecurity.formLogin(Customizer.withDefaults()).build();
+
+		httpSecurity
+				.exceptionHandling((e) -> e.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")));
+
+		return httpSecurity.build();
 	}
 
+	@Bean
+	@Order(2)
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http.formLogin(Customizer.withDefaults());
+
+		http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+
+		return http.build();
+	}
 	@Bean
 	public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
 //		ClientSettings clientSettings = ClientSettings.builder().
 		RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
 				.clientId("taco-admin-client").clientSecret(passwordEncoder.encode("secret"))
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
 				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
 				.redirectUri("http://localhost:8080/login/oauth2/code/taco-admin-client")
 				.scope("writeIngredients").scope("deleteIngredients")
 				.scope(OidcScopes.OPENID)
 				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
 				.build();
+		RegisteredClient registeredClient1 = RegisteredClient.withId(UUID.randomUUID().toString()).clientId("client")
+				// .clientSettings(ClientSettings.builder().requireProofKey(false).build())
+				.clientSecret(passwordEncoder.encode("secret"))
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+				// .redirectUri("http://127.0.0.1:8081/login/oauth2/code/oauth2-authorization-server")
+				.scope(OidcScopes.OPENID).build();
 		// .clientSettings(null).requireUserConsent(true).build();
-		return new InMemoryRegisteredClientRepository(registeredClient);
+		return new InMemoryRegisteredClientRepository(registeredClient, registeredClient1);
 	}
 
 	private static KeyPair generateRsakey() throws NoSuchAlgorithmException {
--- taco-cloud-auth-server/src/main/java/tacos/authorization/SecurityConfig.java
+++ taco-cloud-auth-server/src/main/java/tacos/authorization/SecurityConfig.java
@@ -1,49 +1,48 @@
 package tacos.authorization;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import tacos.service.CustomAuthenticationProvider;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
-	@Autowired
-	private CustomAuthenticationProvider customAuthenticationProvider;
+//	@Autowired
+//	private CustomAuthenticationProvider customAuthenticationProvider;
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-		httpSecurity.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
+		httpSecurity.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
 
 		return httpSecurity.formLogin(Customizer.withDefaults()).build();
 	}
 
-	@Autowired
-	public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
-		authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
+//	@Autowired
+//	public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+//		authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
+//	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
-//	@Bean
-//	UserDetailsService userDetailsService(PasswordEncoder encoder) {
-//		List<UserDetails> list = new ArrayList<>();
-//		list.add(new User("habuma", encoder.encode("password"),
-//				Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-//		list.add(
-//				new User("tacochef", encoder.encode("password"),
-//						Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-//		return new InMemoryUserDetailsManager(list);
-//	}
-
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
+	@Bean
+	public UserDetailsService userDetailsService() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		String password = bCryptPasswordEncoder.encode("12345");
+		UserDetails userDetails = User.withUsername("utk").password(password).roles("USER").build();
+		return new InMemoryUserDetailsManager(userDetails);
+	}
 
 	// Comment 1asdasd
 
